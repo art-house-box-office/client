82f5ca8060aa637506afa477fb90f9aaf5b8acc1
Yvonne Hayes <queyvonne@gmail.com>
2016-07-12 11:17:51 -0700

    started creating form and added account service

---
+++ src/services/account-service.js
@@ -0,0 +1,37 @@
+accountService.$inject = ['$http', 'apiUrl'];
+
+export default function accountService($http, apiUrl) {
+
+  return {
+    // all Companies
+    get() {
+      return $http
+      .get(`${apiUrl}/companies`)
+      .then(r => r.data);
+    },
+    // specific Company
+    getCompany(companyId) {
+      return $http
+      .get(`${apiUrl}/companies/${companyId}`)
+      .then(r => r.data);
+    },
+    // add Company
+    add(company) {
+      return $http
+      .post(`${apiUrl}/company`, company)
+      .then(r => r.data);
+    },
+    // update Company
+    update(companyId) {
+      return $http
+      .update(`${apiUrl}/companies/${companyId}`)
+      .then(r => r.data);
+    },
+    // delete Company
+    delete(companyId) {
+      return $http
+      .delete(`${apiUrl}/companies/${companyId}`)
+      .then(r => r.data);
+    },
+  };
+}
