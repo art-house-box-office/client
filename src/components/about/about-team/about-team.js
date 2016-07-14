a7db4c2ed855132d7d1edad604a89162de8765dd
David <goodwid@gmail.com>
2016-07-14 10:51:26 -0700

    Added about content

--- src/components/about/about-team/about-team.js
+++ src/components/about/about-team/about-team.js
@@ -5,6 +5,37 @@ export default {
   template,
   controller: function () {
     this.style = style;
-
+    this.team = [
+      {
+        name: 'David Goodwin',
+        github: 'https://github.com/goodwid/',
+        linkedIn: 'https://www.linkedin.com/in/david-goodwin-35921b4',
+        bio: 'I traded in my 15 year IT career on a chance at turning my programming hobby into a new career.',
+      },
+      {
+        name: 'team',
+        github: 'https://github.com/',
+        linkedIn: 'https://www.linkedin.com/',
+        bio: '',
+      },
+      {
+        name: 'team',
+        github: 'https://github.com/',
+        linkedIn: 'https://www.linkedin.com/',
+        bio: '',
+      },
+      {
+        name: 'team',
+        github: 'https://github.com/',
+        linkedIn: 'https://www.linkedin.com/',
+        bio: '',
+      },
+      {
+        name: 'team',
+        github: 'https://github.com/',
+        linkedIn: 'https://www.linkedin.com/',
+        bio: '',
+      },
+    ]
   },
 };
