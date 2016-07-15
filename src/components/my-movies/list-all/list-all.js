a8ecfbd8e4b18b320ceab1ab0df535b394d4525e
Johnny Luangphasy <johnny@jluangphasy.com>
2016-07-14 23:26:26 -0700

    Made sidebar smaller

--- src/components/my-movies/list-all/list-all.js
+++ src/components/my-movies/list-all/list-all.js
@@ -3,25 +3,32 @@ import style from './list-all.scss';
 
 export default {
   template,
-  controller: ['$mdDialog', 'screeningService', 'companyService', function ($mdDialog, screeningService, companyService) {
-    this.style = style;
-    this.companyId = companyService.get();
-    screeningService.getByCompany(this.companyId)
-      .then(screenings => {
-        this.screenings = screenings;
-      });
+  controller: [
+    '$mdDialog',
+    'screeningService',
+    'companyService',
+    function controller($mdDialog, screeningService, companyService) {
+      this.style = style;
+      this.companyId = companyService.get();
+      screeningService.getByCompany(this.companyId)
+        .then(screenings => {
+          this.screenings = screenings;
+        });
 
-    this.edit = (index) => {
-      const screening = this.screenings[index];
+      this.edit = (index) => {
+        const screening = this.screenings[index];
 
-      $mdDialog.show({
-        template: '<screening data="screenData"></screening>',
-        controller: ['$scope', function controller($scope) {
-          $scope.screenData = screening;
-        }],
-        clickOutsideToClose: true,
-      });
-    };
-
-  }],
+        $mdDialog.show({
+          template: '<screening data="screenData"></screening>',
+          controller: [
+            '$scope',
+            function controller($scope) { // eslint-disable-line
+              $scope.screenData = screening;
+            },
+          ],
+          clickOutsideToClose: true,
+        });
+      };
+    },
+  ],
 };
