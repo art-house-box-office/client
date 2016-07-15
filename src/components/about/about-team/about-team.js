06b34ddd571ef74ca12651fcaac1f1a5cb257da9
Yvonne Hayes <queyvonne@gmail.com>
2016-07-15 11:32:00 -0700

    changed yvonne bio to be less wordy

--- src/components/about/about-team/about-team.js
+++ src/components/about/about-team/about-team.js
@@ -22,7 +22,7 @@ export default {
         name: 'Yvonne Hayes',
         github: 'https://github.com/YvonneHayes',
         linkedIn: 'https://www.linkedin.com/in/yvonne-hayes',
-        bio: 'I\'m an Austrian who came to Portland via Puerto Vallarta, Mexico and still thinks fondly of the time she lived in London, England. I\'m an avid reader, on again off again runner, gamer, MMORPG addict, catholic school survivor, Netflix binge-watcher and absolute dog person - who is owned by 3 cats (it\'s a long story).I am happiest when working with a team of fellow code enthusiasts hacking away on interesting projects we\'re all passionate about.', // eslint-disable-line
+        bio: 'I\'m an Austrian who came to Portland via Puerto Vallarta, Mexico and still thinks fondly of the time she lived in London, England. I am happiest when working with a team of fellow code enthusiasts hacking away on interesting projects we\'re all passionate about.', // eslint-disable-line
       },
       {
         name: 'David Goodwin',
