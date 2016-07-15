df84720792a66664ad91b8e24d0f9d39c452ae26
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2016-07-15 02:59:12 -0700

    bug fixes and UX UI improvements

--- src/components/my-movies/list-all/list-all.js
+++ src/components/my-movies/list-all/list-all.js
@@ -3,25 +3,26 @@ import style from './list-all.scss';
 
 export default {
   template,
-  controller: ['$mdDialog', 'screeningService', 'companyService', function ($mdDialog, screeningService, companyService) {
-    this.style = style;
-    this.companyId = companyService.get();
-    screeningService.getByCompany(this.companyId)
-      .then(screenings => {
-        this.screenings = screenings;
-      });
-
-    this.edit = (index) => {
-      const screening = this.screenings[index];
-
-      $mdDialog.show({
-        template: '<screening data="screenData"></screening>',
-        controller: ['$scope', function controller($scope) {
-          $scope.screenData = screening;
-        }],
-        clickOutsideToClose: true,
-      });
-    };
-
-  }],
+  controller: ['$mdDialog', 'screeningService', 'companyService', controllerFunc],
 };
+
+function controllerFunc($mdDialog, screeningService, companyService) {
+  this.style = style;
+  this.companyId = companyService.get();
+  screeningService.getByCompany(this.companyId)
+  .then(screenings => {
+    this.screenings = screenings;
+  });
+
+  this.edit = (index) => {
+    const screening = this.screenings[index];
+
+    $mdDialog.show({
+      template: '<screening data="screenData"></screening>',
+      controller: ['$scope', function controller($scope) {
+        $scope.screenData = screening;
+      }],
+      clickOutsideToClose: true,
+    });
+  };
+}
